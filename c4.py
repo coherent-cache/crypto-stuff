192aec4e70faf74c8dbc86578d76a81e704c3789
coherent-cache <rbkv2.0@gmail.com>
2020-05-19 12:31:54 +0900

    Added .gitignore

--- c4.py
+++ c4.py
@@ -1,1 +1,15 @@
-import c3
+import binascii
+from c3 import *
+
+if __name__ == '__main__':
+    with open('4.txt','r') as f:
+        lines = f.readlines()
+
+    output = []
+    for l in lines:
+        decodedinput = binascii.unhexlify(l.strip())
+        output.append(break1bytexor(decodedinput))
+    finalscores = {}
+    for i in output:
+        finalscores[i[0]] = score(i[0])
+    print(max(finalscores, key=finalscores.get))
